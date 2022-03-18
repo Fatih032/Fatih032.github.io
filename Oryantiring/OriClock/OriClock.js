37dfd7c4f88a3c7f62aeb1a2bd35b597079aada1
Fatih Uyanık <fatih03uyanik@gmail.com>
2022-03-18 19:24:50 +0300

    arka plan ve saat renk ayarı eklendi

--- Oryantiring/OriClock/OriClock.js
+++ Oryantiring/OriClock/OriClock.js
@@ -22,14 +22,32 @@ var clock = setInterval(
             sec = "0" + sec;
         }
         if (sec >= 55) {
-            document.getElementById("sonbes").play();
+            document.getElementById("sonbes").play()
         }
         if (sec == 0) {
-            document.getElementById("cikis").play();
+            document.getElementById("cikis").play()
         }
         hour.textContent = hr;
         minute.textContent = min;
         seconds.textContent = sec;
     }, 1000
 );
-
+// $('#arkaplan').click(function () {
+//     var secilenrenk = $("#renk").val();
+//     $(".body").css("background", secilenrenk);
+//     });
+function colorChange(){
+    let secilenrenk = document.getElementById("renk").value;
+    document.body.style.backgroundColor = secilenrenk;
+    document.querySelector("#hour").style.backgroundColor = secilenrenk;
+    document.querySelector("#minute").style.backgroundColor = secilenrenk;
+    document.querySelector("#seconds").style.backgroundColor = secilenrenk;
+}
+function saatRenk(){
+    let saatRenk = document.getElementById("saatRenk").value;
+    document.querySelector("#hour").style.color = saatRenk;
+    document.querySelector("#minute").style.color = saatRenk;
+    document.querySelector("#seconds").style.color = saatRenk;
+    document.querySelector("#span1").style.color = saatRenk;
+    document.querySelector("#span2").style.color = saatRenk;
+}
