79552599bf4ff76e8d76f61de57c3daa5cd9a0f3
Fatih Uyanık <fatih03uyanik@gmail.com>
2022-04-27 02:46:50 +0300

    sporcu kayıt silindi

--- Springboot/src/main/java/com/fatihuyanik/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.fatihuyanik.service;
-
-import com.fatihuyanik.model.User;
-import com.fatihuyanik.dto.UserRegistrationDto;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    User save(UserRegistrationDto registrationDto);
-}
