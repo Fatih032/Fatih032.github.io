8e39386fbeb46a2be393683ac51234cc1f60cce5
Fatih Uyanık <fatih03uyanik@gmail.com>
2022-09-16 21:04:00 +0300

    müzik çalar eklendi

---
+++ MüzikÇalar/music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+
+    {
+        name: "whiplash",
+        artist: "justin hurwitz",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Tourner Dans Le Vide",
+        artist: "Indila",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Jest Oldu",
+        artist: "Karsu",
+        img: "music-3",
+        src: "music-3"
+    },
+/*     {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "music-6"
+    }, */
+    
+    ];
+
+
+// add music 
+/* let favorite = document.querySelector("#favorite");
+let add = document.querySelector("#add");
+
+// add button onclick function
+function addMusic(){
+    let musicName = document.querySelector("#musicName").value;
+    let musicArtist = document.querySelector("#musicArtist").value;
+    let musicImg = document.querySelector("#musicImg").value;
+    let musicSrc = document.querySelector("#musicSrc").value;
+    let music = {
+        name: musicName,
+        artist: musicArtist,
+        img: musicImg,
+        src: musicSrc
+    }
+    allMusic.push(music);
+    console.log(allMusic);
+    alert("Müzik eklendi");
+} */
