2913d9075bb21ab42f9e71106bed48917139cb88
Fatih Uyanık <fatih03uyanik@gmail.com>
2022-09-17 17:07:35 +0300

    kelime tahmin oyunu eklendi

---
+++ Word Scramble Game/js/words.js
@@ -0,0 +1,86 @@
+let words = [
+    {
+        word: "Ekleme",
+        hint: "Sayı ekleme işlemi"
+    },
+    {
+        word: "buluşma",
+        hint: "İnsanların bir araya geldiği olay"
+    },
+    {
+        word: "sayı",
+        hint: "Bir şeyin miktarı"
+    },
+    {
+        word: "tuval",
+        hint: "yağlı boya ile resim yapmak için kullanılan bez"
+    },
+    {
+        word: "Bahçe",
+        hint: "Bitkilerin yetiştiği yer"
+    },
+    {
+        word: "konum",
+        hint: "birinin veya bir şeyin yerleştiği yeri"
+    },
+    {
+        word: "konfor",
+        hint: "hoş bir rahatlama hissi"
+    },
+    {
+        word: "tongue",
+        hint: "The muscular organ of mouth"
+    },
+    {
+        word: "expansion",
+        hint: "The process of increase or grow"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "taste",
+        hint: "Ability of tongue to detect flavour"
+    },
+    {
+        word: "store",
+        hint: "Large shop where goods are traded"
+    },
+    {
+        word: "field",
+        hint: "Area of land for farming activities"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "pocket",
+        hint: "A bag for carrying small items"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "expert",
+        hint: "Person with extensive knowledge"
+    },
+    {
+        word: "statement",
+        hint: "A declaration of something"
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+]
