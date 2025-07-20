10f3ad6636db9958e1bea60046cf5cdbd134d0e0
Fatih032 <fatih03uyanik@gmail.com>
2025-07-20 22:01:48 +0300

    küçük hatalar giderildi

--- Sayaç/scripts.js
+++ Sayaç/scripts.js
@@ -1,6 +1,6 @@
-let seconds = 00;
-let mins = 00;
-let tens = 00;
+let seconds = 0;
+let mins = 0;
+let tens = 0;
 let getSeconds = document.querySelector('.seconds');
 let getTens = document.querySelector('.tens');
 let getMins = document.querySelector('.mins');
@@ -18,38 +18,31 @@ btnStop.addEventListener('click', () => {
 })
 btnReset.addEventListener('click', () => {
     clearInterval(interval);
-    tens = '00';
-    seconds = '00';
-    mins = '00';
-    getSeconds.innerHTML = seconds;
-    getTens.innerHTML = tens;
-    getMins.innerHTML = mins;
+    tens = 0;
+    seconds = 0;
+    mins = 0;
+    updateDisplay();
 })
+
+// Ekranı güncelleyen fonksiyon
+function updateDisplay() {
+    getTens.innerHTML = tens < 10 ? '0' + tens : tens;
+    getSeconds.innerHTML = seconds < 10 ? '0' + seconds : seconds;
+    getMins.innerHTML = mins < 10 ? '0' + mins : mins;
+}
 
 function startTimer() {
     tens++;
-    if(tens <= 9) {
-        getTens.innerHTML = '0' + tens;
-    }
-    if (tens > 9) {
-        getTens.innerHTML = tens;
-    }
-    if ( tens > 99 ) {
+    
+    if (tens > 99) {
         seconds++;
-        getSeconds.innerHTML = '0' + seconds;
         tens = 0;
-        getTens.innerHTML = seconds;
     }
-    if (seconds > 9 ) {
-        getSeconds.innerHTML = seconds;
-    }
-    if(seconds > 59) {
+    
+    if (seconds > 59) {
         mins++;
-        getMins.innerHTML = '0' + mins;
         seconds = 0;
-        getSeconds.innerHTML = '0' + 0;
     }
-    if(mins > 9) {
-        getSeconds.innerHTML = mins;
-    }
+    
+    updateDisplay();
 }
